9e3ad662c49bba1a178de382b5686919846c4d92
Yohan Tremblay <sntremblay@gmail.com>
2023-12-04 13:08:08 -0500

    Azure fonction

---
+++ AzureFonctions/Item.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureFonctions
+{
+    public  class Item
+    {
+        public string id { get; set; }
+        public string category { get; set; }
+         public  string name {get; set; }
+          public   string description { get; set; } 
+       public   bool isComplete { get; set; }   
+           
+
+
+    }
+}
---
+++ AzureFonctions/Program.cs
@@ -0,0 +1,13 @@
+using Microsoft.Extensions.Hosting;
+
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults(builder =>
+    {
+        builder.UseMiddleware<ExceptionHandlingMiddleware>();
+
+    })
+   
+    .Build();
+
+host.Run();
