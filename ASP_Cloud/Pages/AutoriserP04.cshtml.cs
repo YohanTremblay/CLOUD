a7079a4391e63f191d25941e7e7009e24730ae8d
Yohan Tremblay <sntremblay@gmail.com>
2023-12-04 13:03:44 -0500

    asp

--- ASP_Cloud/Pages/AutoriserP04.cshtml.cs
+++ ASP_Cloud/Pages/AutoriserP04.cshtml.cs
@@ -1,6 +1,7 @@
 using ASP_Cloud.Models;
 using ASP_Cloud.Services;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection;
 
 namespace ASP_Cloud.Pages
 {
@@ -8,17 +9,34 @@ namespace ASP_Cloud.Pages
     {
         private CosmosService _cosmosService;
 
+        public IEnumerable<Product>? Products { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            Products ??= await _cosmosService.RetrieveActiveProductsAsync();
+        }
+
         public AutoriserP04Model(ICosmosService cosmosService)
         {
             _cosmosService = new CosmosService();
+
+            // Récupérer les éléments de la dernière heure et les trier par date
+            List<Product> produits = (Products ?? Enumerable.Empty<Product>())
+                                            .OrderBy(p => DateTime.ParseExact(p.Date, "yyyy'-'MM'-'dd'T'HH':'mm':'ss", null))
+                                            .ToList();
+
+            int idLePlusGrand = produits.Max(p => Convert.ToInt32(p.id));
+
             Console.WriteLine("Création Product");
-            Product product = new(
-                id: "20",
+            Product product = new Product
+            (
+                id: Convert.ToString(idLePlusGrand + 1),
                 codeProtocole: "P04",
                 Date: DateTime.Now.ToString("yyyy'-'MM'-'dd'T'HH':'mm':'ss")
             );
-            Console.WriteLine("Envoie");
-            Envoi(product); 
+
+            Console.WriteLine("Envoi");
+            Envoi(product);
         }
 
         private async void Envoi(Product product)
